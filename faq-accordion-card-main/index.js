1b344cc7646d192eadc99b9b7242195770cd3614
nyakita <30837952+nyakita@users.noreply.github.com>
2021-02-14 16:14:47 +0100

    Add files via upload

---
+++ faq-accordion-card-main/index.js
@@ -0,0 +1,92 @@
+
+  document.getElementsByClassName("icon-arrow")[0].onclick = function() {
+
+      var question = document.getElementsByClassName("faq-questions")[0];
+      var element = document.getElementsByClassName("faq-answers")[0];
+      var horizontalLine = document.getElementsByClassName("horizontal-line")[0];
+      element.classList.toggle("toggleElement");
+      question.classList.toggle("bold");
+      horizontalLine.classList.toggle("changePosition");
+
+}
+
+document.getElementsByClassName("faq-questions")[0].onclick = function(){
+  var element = document.getElementsByClassName("faq-answers")[0];
+  var question = document.getElementsByClassName("faq-questions")[0];
+  element.classList.toggle("toggleElement");
+  question.classList.toggle("bold");
+}
+
+document.getElementsByClassName("icon-arrow")[1].onclick = function() {
+
+    var question = document.getElementsByClassName("faq-questions")[1];
+    var element = document.getElementsByClassName("faq-answers")[1];
+    var horizontalLine = document.getElementsByClassName("horizontal-line")[1];
+    element.classList.toggle("toggleElement");
+    question.classList.toggle("bold");
+    horizontalLine.classList.toggle("changePosition");
+
+}
+
+document.getElementsByClassName("faq-questions")[1].onclick = function(){
+  var element = document.getElementsByClassName("faq-answers")[1];
+  var question = document.getElementsByClassName("faq-questions")[1];
+  element.classList.toggle("toggleElement");
+  question.classList.toggle("bold");
+}
+
+document.getElementsByClassName("icon-arrow")[2].onclick = function() {
+
+    var question = document.getElementsByClassName("faq-questions")[2];
+    var element = document.getElementsByClassName("faq-answers")[2];
+    var horizontalLine = document.getElementsByClassName("horizontal-line")[2];
+    element.classList.toggle("toggleElement");
+    question.classList.toggle("bold");
+    horizontalLine.classList.toggle("changePosition");
+
+}
+
+document.getElementsByClassName("faq-questions")[2].onclick = function(){
+  var element = document.getElementsByClassName("faq-answers")[2];
+  var question = document.getElementsByClassName("faq-questions")[2];
+  element.classList.toggle("toggleElement");
+  question.classList.toggle("bold");
+}
+
+
+
+document.getElementsByClassName("icon-arrow")[3].onclick = function() {
+
+    var question = document.getElementsByClassName("faq-questions")[3];
+    var element = document.getElementsByClassName("faq-answers")[3];
+    var horizontalLine = document.getElementsByClassName("horizontal-line")[3];
+    element.classList.toggle("toggleElement");
+    question.classList.toggle("bold");
+    horizontalLine.classList.toggle("changePosition");
+
+}
+
+document.getElementsByClassName("faq-questions")[3].onclick = function(){
+  var element = document.getElementsByClassName("faq-answers")[3];
+  var question = document.getElementsByClassName("faq-questions")[3];
+  element.classList.toggle("toggleElement");
+  question.classList.toggle("bold");
+}
+
+document.getElementsByClassName("icon-arrow")[4].onclick = function() {
+
+    var question = document.getElementsByClassName("faq-questions")[4];
+    var element = document.getElementsByClassName("faq-answers")[4];
+    var horizontalLine = document.getElementsByClassName("horizontal-line")[4];
+    element.classList.toggle("toggleElement");
+    question.classList.toggle("bold");
+    horizontalLine.classList.toggle("changePosition");
+
+}
+
+document.getElementsByClassName("faq-questions")[4].onclick = function(){
+  var element = document.getElementsByClassName("faq-answers")[4];
+  var question = document.getElementsByClassName("faq-questions")[4];
+  element.classList.toggle("toggleElement");
+  question.classList.toggle("bold");
+}
